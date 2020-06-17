2355cb62b3f68480ed26dfbd28526fe1fe459eb0
Romain Barissat <romain-noreply@barissat.com>
2020-06-17 09:14:05 +0700

    Fix babel config and cleanup

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,5 @@
 import { describe, expect, it } from '@jest/globals'
-import { myAsyncName, myName } from '../src/jsModule'
-
-let name = 'John'
+import { myAsyncName, myName } from '../src/index'
 
 /**
  * Sync
@@ -9,14 +7,12 @@ let name = 'John'
 describe('sync tests', function () {
   it('returns valid name (sync)', () => {
     expect.assertions(1)
-    name = 'John'
-    expect(myName(name)).toMatch('John, your name is valid!')
+    expect(myName('John')).toMatch('John, your name is valid!')
   })
 
   it('throws invalid name (sync)', () => {
     expect.assertions(1)
-    name = 'John!'
-    expect(() => myName(name)).toThrow('Your name is invalid')
+    expect(() => myName('John!')).toThrow('Your name is invalid')
   })
 })
 
@@ -26,13 +22,11 @@ describe('sync tests', function () {
 describe('async tests', function () {
   it('returns valid name (async)', async () => {
     expect.assertions(1)
-    name = 'John'
-    expect(await myAsyncName(name)).toMatch('John, your name is valid!')
+    expect(await myAsyncName('John')).toMatch('John, your name is valid!')
   })
 
   it('throws invalid name (async)', async () => {
     expect.assertions(1)
-    name = 'John!'
-    await expect(myAsyncName(name)).rejects.toThrow('Your name is invalid')
+    await expect(myAsyncName('John!')).rejects.toThrow('Your name is invalid')
   })
 })
